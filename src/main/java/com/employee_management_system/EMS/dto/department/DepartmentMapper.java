9ab21f5da2c5070c3294ad5ed1884191c56c0ac9
locoioioi <118866346+locoioioi@users.noreply.github.com>
2024-03-30 12:11:21 +0700

    feat: create Employee DTO

--- src/main/java/com/employee_management_system/EMS/dto/department/DepartmentMapper.java
+++ src/main/java/com/employee_management_system/EMS/dto/department/DepartmentMapper.java
@@ -1,8 +1,15 @@
 package com.employee_management_system.EMS.dto.department;
 
 import com.employee_management_system.EMS.entity.Department;
+import com.employee_management_system.EMS.entity.Employee;
+import com.employee_management_system.EMS.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 
+import java.util.HashSet;
+
+@RequiredArgsConstructor
 public class DepartmentMapper {
+    private EmployeeRepository employeeRepository;
     public DepartmentDTO toDTO(Department department) {
         if (department == null) return null;
         return new DepartmentDTO(
@@ -10,6 +17,17 @@ public class DepartmentMapper {
                 department.getName(),
                 department.getEmployee().getId(),
                 department.getEmployees().size()
+        );
+    }
+
+    public Department toDepartment(CreationDepartment department) {
+        Employee employee = employeeRepository.findById(department.getManagerId()).orElseThrow();
+        return new Department(
+                0,
+                department.getName(),
+                employee,
+                new HashSet<>(),
+                new HashSet<>()
         );
     }
 }
