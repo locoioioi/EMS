3c542c2b2ee925a28ef40e6c64f4880d06b81eae
locoioioi <118866346+locoioioi@users.noreply.github.com>
2024-04-03 11:55:45 +0700

    feat: security

--- src/main/java/com/employee_management_system/EMS/security/SecurityConfig.java
+++ src/main/java/com/employee_management_system/EMS/security/SecurityConfig.java
@@ -3,8 +3,18 @@ package com.employee_management_system.EMS.security;
 import com.employee_management_system.EMS.service.user.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -17,5 +27,26 @@ public class SecurityConfig {
         DaoAuthenticationProvider dap = new DaoAuthenticationProvider();
         dap.setUserDetailsService(userService);
         return dap;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                request -> request.requestMatchers("*").permitAll()
+        );
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(
+                cors -> cors
+                        .configurationSource(
+                                request -> {
+                                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                                    corsConfiguration.addAllowedOrigin("*");
+                                    corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                                    corsConfiguration.addAllowedHeader("*");
+                                    return corsConfiguration;
+                                }
+                        )
+                );
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 }
