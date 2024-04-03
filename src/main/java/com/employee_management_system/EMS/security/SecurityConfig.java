27abd17634f2ab561ac0983b63d2648543c4ee25
locoioioi <118866346+locoioioi@users.noreply.github.com>
2024-04-03 10:12:05 +0700

    feat: set up Dao Authentication provider and Bcrypt Password encoder

---
+++ src/main/java/com/employee_management_system/EMS/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.employee_management_system.EMS.security;
+
+import com.employee_management_system.EMS.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider provider(UserService userService) {
+        DaoAuthenticationProvider dap = new DaoAuthenticationProvider();
+        dap.setUserDetailsService(userService);
+        return dap;
+    }
+}
